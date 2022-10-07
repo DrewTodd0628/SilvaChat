7347918396fb9c86ac53e08a5acfe18151f27952
BuildTools <unconfigured@null.spigotmc.org>
2022-10-07 12:38:41 -0400

    Made some touches to the interface

--- CRUD_2/Spring-API/demo/frontend/src/components/auth-nav.js
+++ CRUD_2/Spring-API/demo/frontend/src/components/auth-nav.js
@@ -2,9 +2,9 @@ import React from 'react';
 import AuthenticationButton from './authentication-button';
 
 const AuthNav = () => (
-  <div className="navbar-nav ml-auto">
+  <>
     <AuthenticationButton />
-  </div>
+  </>
 );
 
 export default AuthNav;
--- CRUD_2/Spring-API/demo/frontend/src/components/nav-bar.js
+++ CRUD_2/Spring-API/demo/frontend/src/components/nav-bar.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './style/nav.css';
 
 import AuthNav from './auth-nav';
+import { useHistory } from 'react-router-dom';
 
 const NavBar = () => {
+  const history = useHistory();
+
   return (
     <div className="nav-container mb-3">
       <nav className="navbar navbar-expand-md navbar-light bg-light">
         <div className="container">
           <div className="navbar-brand logo" />
           <h2 className='logo'>SilvaChat</h2>
-          <AuthNav />
+          <div className='navButtons'>
+            <button className='navBtn btn btn-primary'  onClick={() => {history.push("/chat")}}>Chat</button>
+            <AuthNav />
+          </div>
           
         </div>
       </nav>
